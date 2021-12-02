85e1f385d256ddbea444d9d739fdfc9fb16b4cb3
androborn <andrii.titov@gmail.com>
2021-12-02 23:59:13 +0200

    Build core file structure
    *Add core file structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
-import './js/modules';
+import x from './js/modules/http-services/themoviedb-api.js';
+import y from './js/modules/components/page-switch.js';
+import z from './js/modules/templates/set-card-markup.js';
