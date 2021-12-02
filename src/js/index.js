427e3749817c5c322476fcd11ac9cdaa5626cdde
androborn <andrii.titov@gmail.com>
2021-12-03 00:04:29 +0200

    Fix file pathes
    *Fix erroneous files inclides and imports pathes in index.htms and index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
-import x from './js/modules/http-services/themoviedb-api.js';
-import y from './js/modules/components/page-switch.js';
-import z from './js/modules/templates/set-card-markup.js';
+import x from './modules/http-services/themoviedb-api.js';
+import y from './modules/components/page-switch.js';
+import z from './modules/templates/set-card-markup.js';
