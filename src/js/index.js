592ca44542fd69bc8659d99a6ec608b8d834ad78
androborn <andrii.titov@gmail.com>
2021-12-04 19:41:29 +0200

    [regfactor] Change file structure
    *Add themoviedb-api.js
    *Move code from fetchMovies.js to themoviedb-api.js
    *Remove fetchMovies.js
    *Edit import in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
-import x from './modules/http-services/themoviedb-api.js';
 import y from './modules/components/page-switch.js';
 import z from './modules/templates/set-card-markup.js';
-import { fetchMovies } from '/js/modules/http-services/fetch-movies';
 
+import { themoviedbApi } from '/js/modules/http-services/themoviedb-api';
---
+++ src/js/modules/http-services/themoviedb-api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+export class themoviedbApi {
+  constructor() {
+    this.API_KEY = 'f979c5d182d240e48ce15515a9f4d973';
+    this.keyword = '';
+    axios.defaults.baseURL = 'https://api.themoviedb.org/';
+  }
+  async getMovies() {
+    try {
+      const response = await axios.get(
+        `/3/trending/all/day?api_key=${this.API_KEY}`,
+      );
+      const data = await response.data;
+      return data; //{page: 1, results: Array(20), total_pages: 1000, total_results: 20000}
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  get search() {
+    return this.keyword;
+  }
+  set search(newKeyword) {
+    this.keyword = newKeyword;
+  }
+  async getMoviesByKeyword() {
+    try {
+      const response = await axios.get(
+        `/3/search/movie?api_key=${this.API_KEY}&query=${this.keyword}`,
+      );
+      const data = await response.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
