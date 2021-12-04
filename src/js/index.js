fe5aaebf849cc27bd62c841ef2b65994837b8f6d
codajoker <codajoker91@gmail.com>
2021-12-04 21:25:06 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,3 @@ import x from './modules/http-services/themoviedb-api.js';
 import y from './modules/components/page-switch.js';
 import z from './modules/templates/set-card-markup.js';
 import { fetchMovies } from '/js/modules/http-services/fetch-movies';
-
-import q from './vendors/_vendors';
