b4ed5339e5026a31509a616d499a7994f7b7e887
Andrii Titov <85036680+Androborn@users.noreply.github.com>
2021-12-05 11:37:05 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import y from './modules/components/page-switch.js';
 import z from './modules/templates/set-card-markup.js';
 
-import Loader from '/js/vendors/icon8.js';
+import Loader from '/js/vendors/_icon8.js';
 import { ThemoviedbApi } from '/js/modules/http-services/themoviedb-api.js';
