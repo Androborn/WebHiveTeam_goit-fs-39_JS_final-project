233d55c47ef76193578b42cd8f2ddc151bdd2054
Bogdan <ortbg1988@gmail.com>
2021-12-05 14:06:06 +0200

    [feat] Create a new class LibraryStorage
    *JS code*
    -add a new class - LibraryStorage (create a library with films in a localStorage);
    - add the class methods: getStorageList(get values from a localeStorage),  addToStorage (add a film to library by id); removeFromStorage(remove a film from library by id);
    - add getter (take the library key) and setter (change the library key);
    - add the import to the index.js.

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import y from './modules/components/page-switch.js';
 import z from './modules/templates/set-card-markup.js';
 
 import { ThemoviedbApi } from '/js/modules/http-services/themoviedb-api';
+import { LibraryStorage } from './modules/components/library-storage.js';
