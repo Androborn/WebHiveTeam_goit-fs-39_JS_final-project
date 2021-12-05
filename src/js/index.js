9c00e8d9d29f12aba4b5a084962e8d4d4f06c6a6
androborn <andrii.titov@gmail.com>
2021-12-05 15:34:22 +0200

    [fix] UPD index.js
    *Remove excessive import of ThemoviedbApi class

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 // import z from './modules/templates/set-card-markup.js';
 
 import { Header } from './modules/components/page-switch.js';
-import { ThemoviedbApi } from '/js/modules/http-services/themoviedb-api';
 import { ThemoviedbApi } from '/js/modules/http-services/themoviedb-api.js';
 import { LibraryStorage } from './modules/components/library-storage.js';
 
