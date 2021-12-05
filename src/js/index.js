b79eb7a000a7e97e8e67fe83c035ac306c6b7ecb
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2021-12-05 13:09:11 +0200

    [feat] Create a header for the "My Library" page
    HTML-markup:
    - Add data-attributes to nav-links
    - Add class "header-serch__wrapper-lib"
    Styles:
    - Add styles to common-button.scss
    - Add styles to common-button modificator
    JS:
    - Add class Header and add some methods
    - Add callback function for tracking current page

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-import y from './modules/components/page-switch.js';
+import { Header } from './modules/components/page-switch.js';
 import z from './modules/templates/set-card-markup.js';
 
 import { ThemoviedbApi } from '/js/modules/http-services/themoviedb-api';
+
+const header = new Header();
