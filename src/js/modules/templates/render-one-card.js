d854163a6a27ed3a0a1ea3812e3b484c4f6a8086
WebGertsog <groxenvil@gmail.com>
2021-12-07 20:24:49 +0200

    [feet] Display first two genres and “others”
    1. Create a method that checks if the genre length is more than three words then reduce the list of genres to two and display the word "other ..." instead of the third genre;
    2. Create a method that takes the full release date of the movie and returns only the release year;
    3. Create a method that prevents the line of the title of the movie from wrapping to another line

--- src/js/modules/templates/render-one-card.js
+++ src/js/modules/templates/render-one-card.js
@@ -1,4 +1,5 @@
 import { ThemoviedbApi } from "../http-services/themoviedb-api";
+
 const moviesApi = new ThemoviedbApi();
 const GENRES_LIST_KEY = "Genres list";
 moviesApi.getMoviesGenresList().then(list => {
@@ -9,45 +10,65 @@ export class createCardsMarkup {
   constructor(cards, page) {
     this.cards = cards;
     this.page = page;
+    this.moviesApi = new ThemoviedbApi()
     this.genres = JSON.parse(localStorage.getItem(GENRES_LIST_KEY));
-    
   }
+
   getMovieGenresName(genreId) {
     const filmGenre = this.genres.find(genre => genre.id === genreId);
       return filmGenre.name;
   }
   createGenresList(genresId) {
-    const genresNames = genresId.map(id => this.getMovieGenresName(id)).join(', ');
-    return genresNames;
+    const genresNames = genresId.map(id => this.getMovieGenresName(id));
+
+    if(genresNames.length >= 4) {
+      const CorrecТame = genresNames.slice(0, 2);
+      CorrecТame.push('Other...');
+      return CorrecТame.join(', ');
+    }
+    return genresNames.join(', ');
   }
+  
+  correctedDate(release_date) {
+    return release_date.slice(0, 4);
+  }
+
+  slicedOriginalTitle(original_title){
+    if(original_title.length >= 35) {
+      const slisedTitle = original_title.slice(0, 35);
+      return slisedTitle + " ...";
+    }
+    return original_title
+  }
+
 createCard() {
    if (this.page === "main") {
-     return this.cards.map(({ id, poster_path, original_title, genre_ids, release_date, original_name }) => 
+     return this.cards.map(({id, poster_path, original_title, genre_ids, release_date}) => 
       `<li class="cards-list__item" id="${id}">
         <img class="cards-list__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" />
         <div class="cards-list__info">
-          <h2 class="cards-list__info-name">${original_name || original_title}</h2>
+          <h2 class="cards-list__info-name">${this.slicedOriginalTitle(original_title)}</h2>
           <div class="card-list__description">
-            <p class="cards-list__genre">${this.createGenresList(genre_ids)}</p>
-            <p class="cards-list__date">${release_date}</p>
+             <p class="cards-list__genre">${this.createGenresList(genre_ids)}</p>
+            <p class="cards-list__date">| ${this.correctedDate(release_date)}</p>
           </div>
         </div>
       </li>`
-    ).join('');
+     ).join('');
     };
-      
       return this.cards.map(({id, poster_path, original_title, genre_ids, release_date, vote_average}) =>
       `<li class="cards-list__item" id="${id}">
         <img class="cards-list__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" />
         <div class="cards-list__info">
-          <h2 class="cards-list__info-name">${original_title}</h2>
+          <h2 class="cards-list__info-name">${this.slicedOriginalTitle(original_title)}</h2>
           <div class="card-list__description">
-            <p class="cards-list__genre">${genre_ids}</p>
-            <p class="cards-list__date">${release_date}</p>
+             <p class="cards-list__genre">${this.createGenresList(genre_ids)}</p>
+            <p class="cards-list__date">| ${this.correctedDate(release_date)}</p>
             <span class="cards-list__rating">${vote_average}</span>
           </div>
         </div>
       </li>`
-    ).join('');
+      ).join('');
  };
 }
+
