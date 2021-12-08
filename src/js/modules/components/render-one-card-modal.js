5303e7fe101e3d1bf1cfff98b9ae56a6dd1266a8
androborn <andrii.titov@gmail.com>
2021-12-09 00:46:41 +0200

    [refactor] Move markup generation to a separate module
    *Rename set-card-markup.js to modal-markup.js
    *Remove modal window markup from render-one-card-modal.js
    *Add modal window markup to modal-markup.js
    *Import modal-markup.js to render-one-card-modal.js and call

--- src/js/modules/components/render-one-card-modal.js
+++ src/js/modules/components/render-one-card-modal.js
@@ -1,4 +1,5 @@
 import { ThemoviedbApi } from '../http-services/themoviedb-api';
+import { modalMarkup } from '../templates/modal-markup';
 import { LibraryStorage } from './library-storage';
 
 export class RenderModal {
@@ -24,81 +25,21 @@ export class RenderModal {
     if (!iscardsList) {
       return;
     }
-    console.log(cardsList);
-    console.log(this.cardsListId);
 
     const data = await this.themoviedbApi.getMovieById(this.cardsListId);
     const genre = data.genres.map(id => id.name);
     const genreIds = Object.values(genre).join(', ');
 
-    this.instance.element().innerHTML = `
-    <div class="modal">
-      <div class="modal__wraper"><div class="modal__image-wraper">
-        <img
-          src="https://image.tmdb.org/t/p/w500${data.poster_path}"
-          alt="tags"
-          loading="lazy"
-          class="modal__image"
-        />
-      </div>
-      <div class="modal__info">
-        <h1 class="modal__info-title">${data.original_title.toUpperCase()}</h1>
-        <div class="modal__info-wraper">
-          <div class="modal__info-item-wraper">
-            <span class="modal__info-item">
-              Vote <span class="modal__info-slash"> / </span>
-              <span class="modal__info-item"> Votes </span></span
-            >
-            <span class="modal__info-value modal__info-value--hight">
-              <span class="modal__info-value  modal__info-value--border">${
-                data.vote_average
-              }</span>
-              <span class="modal__info-slash"> / </span>${data.vote_count}</span
-            >
-          </div>
-          <div class="modal__info-item-wraper">
-            <span class="modal__info-item"> Popularity </span>
-            <span class="modal__info-value modal__info-value--hight">${data.popularity.toFixed(
-              1,
-            )}</span>
-          </div>
-          <div class="modal__info-item-wraper">
-            <span class="modal__info-item"> Original Title </span>
-            <span class="modal__info-value">
-              ${data.original_title.toUpperCase()}</span></div>
-          <div class="modal__info-item-wraper">
-            <span class="modal__info-item"> Genre </span
-            ><span class="modal__info-value">${genreIds}</span>
-          </div>
-        </div>
-        <div class="modal__info-about-wraper">
-          <p class="modal__info-about">
-            ABOUT<span class="modal__info-value modal__info-value--hight">${
-              data.overview
-            }</span>
-          </p>
-        </div>
-      </div><div class="modal__btn-wraper">
-          <button
-            class="common-btn common-btn--active modal__btn-watched"
-            data-action="add-to-watched"
-            data-id="${this.cardsListId}"
-          >
-            ADD TO WATCHED</button
-          ><button
-            class="common-btn modal__btn-queue"
-            data-action="add-to-queue"
-            data-id="${this.cardsListId}"
-          >
-            ADD TO QUEUE
-          </button>
-        </div></div>
-    </div>
-    `;
+    this.instance.element().innerHTML = modalMarkup(
+      data,
+      genreIds,
+      this.cardsListId,
+    );
 
     this.btnWatched = this.instance
       .element()
       .querySelector('[data-action="add-to-watched"]');
+
     this.btnWatched.addEventListener('click', event =>
       this.onBtnWatchedClick(event),
     );
@@ -106,6 +47,7 @@ export class RenderModal {
     this.btnQueue = this.instance
       .element()
       .querySelector('[data-action="add-to-queue"]');
+
     this.btnQueue.addEventListener('click', event =>
       this.onBtnQueueClick(event),
     );
@@ -134,9 +76,3 @@ export class RenderModal {
     this.btnQueue.disabled = true;
   }
 }
-
-`<span class="list_text">
-  <span class="list_text_vote">7.2</span>
-  <span class="list_text_vote_count">/</span>
-  4097
-</span>;`;
---
+++ src/js/modules/templates/modal-markup.js
@@ -0,0 +1,66 @@
+export function modalMarkup(data, genreIds, cardsListId) {
+  return `
+    <div class="modal">
+      <div class="modal__wraper"><div class="modal__image-wraper">
+        <img
+          src="https://image.tmdb.org/t/p/w500${data.poster_path}"
+          alt="tags"
+          loading="lazy"
+          class="modal__image"
+        />
+      </div>
+      <div class="modal__info">
+        <h1 class="modal__info-title">${data.original_title.toUpperCase()}</h1>
+        <div class="modal__info-wraper">
+          <div class="modal__info-item-wraper">
+            <span class="modal__info-item">
+              Vote <span class="modal__info-slash"> / </span>
+              <span class="modal__info-item"> Votes </span></span
+            >
+            <span class="modal__info-value modal__info-value--hight">
+              <span class="modal__info-value  modal__info-value--border">${
+                data.vote_average
+              }</span>
+              <span class="modal__info-slash"> / </span>${data.vote_count}</span
+            >
+          </div>
+          <div class="modal__info-item-wraper">
+            <span class="modal__info-item"> Popularity </span>
+            <span class="modal__info-value modal__info-value--hight">${data.popularity.toFixed(
+              1,
+            )}</span>
+          </div>
+          <div class="modal__info-item-wraper">
+            <span class="modal__info-item"> Original Title </span>
+            <span class="modal__info-value">
+              ${data.original_title.toUpperCase()}</span></div>
+          <div class="modal__info-item-wraper">
+            <span class="modal__info-item"> Genre </span
+            ><span class="modal__info-value">${genreIds}</span>
+          </div>
+        </div>
+        <div class="modal__info-about-wraper">
+          <p class="modal__info-about">
+            ABOUT<span class="modal__info-value modal__info-value--hight">${
+              data.overview
+            }</span>
+          </p>
+        </div>
+      </div><div class="modal__btn-wraper">
+          <button
+            class="common-btn common-btn--active modal__btn-watched"
+            data-action="add-to-watched"
+            data-id="${cardsListId}"
+          >
+            ADD TO WATCHED</button
+          ><button
+            class="common-btn modal__btn-queue"
+            data-action="add-to-queue"
+            data-id="${cardsListId}"
+          >
+            ADD TO QUEUE
+          </button>
+        </div></div>
+    </div>
+    `;
+}
