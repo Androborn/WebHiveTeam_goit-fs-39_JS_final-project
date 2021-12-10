69992704cacb142e1b93b91d28dd27caf9e53bd6
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2021-12-10 23:33:43 +0200

    [feat] Team modal window
    - Add html markup for the modal window
    - Add modal window styles
    - Add styles for breakpoints
    - I add additional links to the refs.js file
    - Add social media icons to the sprite.svg
    - Created a file team-modal-js to open and close a modal window
    - Add import of file team-modal.js to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import './modules/components/page-switch';
 import './modules/components/render-one-card-modal';
 import { movieService } from './modules/components/movie-service';
+import './modules/components/team-modal';
 
 // import Loader from './vendors/_icon8';
 
