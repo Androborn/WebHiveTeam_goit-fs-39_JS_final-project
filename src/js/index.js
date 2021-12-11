52d3396b57d04cb98ec9ca380bff51d867790956
WebGertsog <groxenvil@gmail.com>
2021-12-11 15:28:49 +0200

    [FT-tech]-Create-an-anchor-button-on-the-main-page
    1. Create a link in main-page.html that scrolls the page to the title of the page;
    2. Draw a button and save the image in "svg" format;
    3. Add "svg" to the file "sprite.svg";
    4. Add the optimized "svg" element to the link in the "main-page.html" file;
    5. Create a style file "anchor-button.scss" and include it in the file "main.scss";
    6. Write the basic styles of the anchor button and write the styles for the breakpoints (320px, 375px, 393px, 425px, 768px, 1024px, 1440px) in the file "anchor-button.scss";
    7. Write additional styles for hover and focus on the button
    8. Create a file "anchor-button.js" and include it in the file "index.js";
    9. Write the logic for the anchor button:
    9.1. When the body of the document or the root element of the document is scrolled up by 250px then the class "anch-btn-active" will be added to the element with the class ".anchor-btn". In all other cases, the ".anchor-btn" class will be removed;
    9.2. When you click on the button with the ".anchor-btn" class, smooth scrolling to the top of the page should be performed.

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import './modules/components/page-switch';
 import './modules/components/render-one-card-modal';
 import { movieService } from './modules/components/movie-service';
 import './modules/components/team-modal';
+import './modules/components/anchor-btn';
 
 // import Loader from './vendors/_icon8';
 
