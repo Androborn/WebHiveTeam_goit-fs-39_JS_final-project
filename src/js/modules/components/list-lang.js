b984c8d97bb871b4c589456e6b79ecd1eaf8a4ac
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2021-12-14 15:34:04 +0200

    [feat] Language-switching
    - Add translation on the main page and in the modal window
    - Add translation of Home and My Library buttons

---
+++ src/js/modules/components/list-lang.js
@@ -0,0 +1,12 @@
+export const langObj = {
+    "btn-home": {
+        "ru": "Главная",
+        "en": "HOME",
+        "ua": "Головна",
+    },
+    "btn-lib": {
+        "ru": "Моя библиотека",
+        "en": "my library",
+        "ua": "Моя бiблiотека",
+    },
+}
